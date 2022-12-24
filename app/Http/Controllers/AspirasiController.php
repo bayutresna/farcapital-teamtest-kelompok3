11360c842c0848fbf2eb18ef1fb8a887bdf6b905
endjiansyah <endji1507@gmail.com>
2022-12-24 10:50:30 +0700

    use AspirasiController di Route web

--- app/Http/Controllers/AspirasiController.php
+++ app/Http/Controllers/AspirasiController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\HttpClient;
 use Illuminate\Http\Request;
 
 class AspirasiController extends Controller
 {
-    //
+    function index()
+    {
+        $aspirasi = HttpClient::fetch(
+            "GET",
+            "http://localhost:8000/api/aspirasi/list"
+        );
+        $data = $aspirasi["data"];
+
+        return view('aspirasi.index', [
+            "data" => $data
+        ]);
+    }
+
+    function detail($id)
+    {
+        $linknya = "http://localhost:8000/aspirasi/" . $id . "/show";
+        $responseData = HttpClient::fetch(
+            "GET",
+            $linknya
+        );
+        $data = $responseData["data"];
+
+        return view('aspirasi.detail', [
+            "data" => $data
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AspirasiController;
 use Illuminate\Support\Facades\Route;
 
 /*
