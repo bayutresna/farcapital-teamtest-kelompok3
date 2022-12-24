d2eda07539397414dbe3b2e558ad1a490537eb75
Bayu Tresna <bayutresna2000@gmail.com>
2022-12-24 13:24:50 +0700

    add middleware and fix kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'withauth' => \App\Http\Middleware\WithAuth::class,
+        'noauth' => \App\Http\Middleware\NoAuth::class,
     ];
 }
