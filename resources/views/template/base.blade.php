dc8681f337a5bf6e5899ec91777cd11c9b59b206
diahnkha <diahnurkhasanah5@gmail.com>
2022-12-24 14:25:36 +0700

    add changes on base views

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -24,10 +24,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/admin">Home</a>
                     </li>
 
                     @if(!session()->get("logged", false))
