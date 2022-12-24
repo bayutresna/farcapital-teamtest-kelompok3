143eeed39d18aff90e5761b5848b37b33da42e20
endjiansyah <endji1507@gmail.com>
2022-12-24 13:53:57 +0700

    perbaikan view user.blade

--- resources/views/frontend/user/index.blade.php
+++ resources/views/frontend/user/index.blade.php
@@ -1,44 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('template.base')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
+@section('konten')
+    <div class="card">
+        <div class="card-header">
+            <a href="{{ route('user.create') }}" class="btn btn-secondary btn-sm">Tambah Admin</a>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Opsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
 
-<body>
-    <button> <a href="/user/create">Add</a> </button>
-    <table>
-        <thead>
-            <tr>
-                <th>Nama</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Password</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                <tr>
-                    <td>
-                    <td>{{ $user['nama'] }}</td>
-                    <td>{{ $user['username'] }}</td>
-                    <td>{{ $user['email'] }}</td>
-                    <td>{{ $user['password'] }}</td>
-                    <td><button> <a href="/user/detail/{{ $user['id'] }}">Edit</a></button></td>
-                    <td>
-                        <form action="{{ route('user.destroy', ['id' => $user['id']]) }}" method="post">
-                            @csrf
-                            <button class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-</body>
-
-</html>
+                            <td>{{ $user['nama'] }}</td>
+                            <td>{{ $user['username'] }}</td>
+                            <td>{{ $user['email'] }}</td>
+                            <td>
+                                <form action="{{ route('user.destroy', ['id' => $user['id']]) }}" method="post">
+                                    <a href="/user/detail/{{ $user['id'] }}" class="btn btn-primary btn-sm">Edit</a>
+                                    @csrf
+                                    <button class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
